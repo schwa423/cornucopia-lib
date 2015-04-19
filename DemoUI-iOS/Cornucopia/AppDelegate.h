a9c0fddf9669543ebc0856c1d06be688733527c8
Josh Gargus <josh@schwa.ca>
2015-04-19 12:55:02 -0700

    Create empty XCode project.

---
+++ DemoUI-iOS/Cornucopia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cornucopia
+//
+//  Created by Josh Gargus on 4/19/15.
+//  Copyright (c) 2015 Foo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoUI-iOS/Cornucopia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cornucopia
+//
+//  Created by Josh Gargus on 4/19/15.
+//  Copyright (c) 2015 Foo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
